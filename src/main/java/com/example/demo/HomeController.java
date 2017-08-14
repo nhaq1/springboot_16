cd65da92259e43417fe31e6733087977f5dc4249
nhaq1 <nhaq1@yahoo.com>
2017-08-14 19:01:45 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by oracle on 8/14/17.
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/2")
+    public String page2(){
+        return "page2";
+    }
+
+    @RequestMapping("/3")
+    public String page3(){
+        return "page3";
+    }
+}
